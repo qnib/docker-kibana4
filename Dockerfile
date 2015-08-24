cfe3c319554c79c6222411afc604643df262758c
Christian Kniep <ckniep@gaikai.com>
2015-08-24 17:23:33 +0200

    add ES-datacenter and refine restore script

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,13 @@ RUN curl -s -L -o kibana-${KIBANA_VER}-linux-x64.tar.gz https://download.elastic
     rm /opt/kibana*.tar.gz
 RUN ln -sf /opt/kibana-${KIBANA_VER}-linux-x64 /opt/kibana
 ADD etc/supervisord.d/kibana.ini /etc/supervisord.d/
+ADD opt/qnib/kibana/bin/start_kibana.sh /opt/qnib/kibana/bin/
 ADD etc/consul.d/check_kibana.json /etc/consul.d/check_kibana.json
 # Config kibana
 ADD opt/kibana/config/kibana.yml /opt/kibana/config/kibana.yml
 ## Dashboard
-RUN yum install -y npm jq bc && \
-    npm install elasticdump -g
+RUN yum clean all;yum install -y npm jq bc 
+RUN npm install elasticdump -g
 ADD etc/supervisord.d/kibana_restore.ini /etc/supervisord.d/
 ADD opt/qnib/bin/kibana_restore.sh /opt/qnib/bin/
 ADD dumps/ /opt/kibana4_dumps/
--- opt/qnib/bin/kibana_restore.sh
+++ opt/qnib/bin/kibana_restore.sh
@@ -7,7 +7,7 @@ function wait_es {
         echo "ES startup took to long..."
         exit 42
     fi
-    if [ "x$(curl -s http://192.168.99.100:9200/ |jq .status)" != "x200" ];then
+    if [ "x$(curl -s http://172.17.42.1:9200/ |jq .status)" != "x200" ];then
         sleep 1
         wait_es
     else
@@ -18,18 +18,23 @@ function wait_es {
 wait_es
 sleep 5
 
-if [ -f /opt/kibana4_dumps/mapping.json ];then
-   elasticdump \
-     --bulk=true \
-     --input=/opt/kibana4_dumps/mapping.json \
-     --output=http://elasticsearch.service.consul:9200/
-fi
+kibana_size=$(curl -s http://172.17.42.1:9200/.kibana/_status|jq '.indices[".kibana"].index.size_in_bytes')
 
-if [ -f /opt/kibana4_dumps/data.json ];then
-   elasticdump \
-     --bulk=true \
-     --input=/opt/kibana4_dumps/data.json \
-     --output=http://elasticsearch.service.consul:9200/
+if [ "Xnull" == "X$kibana_size" ];then
+
+    if [ -f /opt/kibana4_dumps/mapping.json ];then
+        elasticdump \
+         --bulk=true \
+         --input=/opt/kibana4_dumps/mapping.json \
+         --output=http://elasticsearch.service.consul:9200/
+    fi
+
+    if [ -f /opt/kibana4_dumps/data.json ];then
+        elasticdump \
+         --bulk=true \
+         --input=/opt/kibana4_dumps/data.json \
+         --output=http://elasticsearch.service.consul:9200/
+    fi
 fi
 
 exit 0
