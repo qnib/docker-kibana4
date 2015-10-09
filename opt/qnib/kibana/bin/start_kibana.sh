dc58c0a03f096b0df6c64bbbf1abd62bdfbe2852
Christian Kniep <christian@qnib.org>
2015-10-09 12:58:29 +0200

    added ES_URL as environment setting

--- opt/qnib/kibana/bin/start_kibana.sh
+++ opt/qnib/kibana/bin/start_kibana.sh
@@ -3,5 +3,8 @@
 if [ "X${ES_DC}" != "X" ];then
    sed -i'' -e "s#elasticsearch_url:.*#elasticsearch_url: \"http://elasticsearch.service.${ES_DC}.consul:9200\"#" /opt/kibana/config/kibana.yml
 fi
+if [ "X${ES_URL}" != "X" ];then
+   sed -i'' -e "s#elasticsearch_url:.*#elasticsearch_url: \"${ES_URL}\"#" /opt/kibana/config/kibana.yml
+fi
 
 /opt/kibana/bin/kibana --config /opt/kibana/config/kibana.yml
